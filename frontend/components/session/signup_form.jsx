2135f74983560d986ce638f57c6ea2cace89513f
Rex Bodoia <rex@bodoia.com>
2018-08-09 14:42:13 -0700

    Render basic errors styling

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -17,6 +17,7 @@ class SignupForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.update = this.update.bind(this);
     this.handleDemo = this.handleDemo.bind(this);
+    this.errors = this.errors.bind(this);
   }
 
   handleSubmit(e) {
@@ -36,6 +37,19 @@ class SignupForm extends React.Component {
     this.props.demoLogin(user);
   }
 
+  errors() {
+    console.log(this.props.errors);
+    if(this.props.errors.length !== 0) {
+      return (
+        <ul className='errors'>
+        {this.props.errors.map((error, idx) => {
+          return <li key={idx}>{error}</li>
+        })}
+        </ul>
+      );
+    }
+  }
+
   update(field){
     return (e) => {
       this.setState({[field]: e.target.value});
@@ -47,6 +61,7 @@ class SignupForm extends React.Component {
       <div>
         <SignupHeader history={this.props.history}/>
 
+        {this.errors()}
         <div className='auth-container'>
           <div>
             <h1 className='signup-form-header'>Create your account</h1>
@@ -56,19 +71,19 @@ class SignupForm extends React.Component {
               <h1 className='form-header last-name-header'>Last Name</h1>
 
               <div className='name-container signup-input-container'>
-                <input type='text' onChange={this.update('f_name')} placeholder='First Name' className='first-name-input signup-input' required></input>
+                <input type='text' onChange={this.update('f_name')} placeholder='First Name' className='first-name-input signup-input'></input>
 
-                <input type='text' onChange={this.update('l_name')} placeholder='Last Name' className='last-name-input signup-input' required></input>
+                <input type='text' onChange={this.update('l_name')} placeholder='Last Name' className='last-name-input signup-input'></input>
               </div>
 
               <div className='signup-input-container'>
                 <h1 className='form-header'>Email</h1>
-                <input type='email' onChange={this.update('email')} placeholder='Your email address' className='signup-input' required></input>
+                <input type='email' onChange={this.update('email')} placeholder='Your email address' className='signup-input'></input>
               </div>
 
               <div className='signup-input-container'>
                 <h1 className='form-header'>Password</h1>
-                <input type='password' onChange={this.update('password')} placeholder='Choose a password' className='signup-input' required></input>
+                <input type='password' onChange={this.update('password')} placeholder='Choose a password' className='signup-input'></input>
               </div>
 
               <input type='submit' value='Create account' className='signup-button'></input>
@@ -80,12 +95,6 @@ class SignupForm extends React.Component {
               <h1>Already have a Coincenter account?</h1>
               <Link className='login-link' to='/signin'>Log in</Link>
             </div>
-
-            <ul>
-              {this.props.errors.map((error, idx) => {
-                return <li key={idx}>{error}</li>
-              })}
-            </ul>
           </div>
         </div>
       </div>
