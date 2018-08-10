69bde76f5935469187cc1302989e397a809fda41
Rex Bodoia <rex@bodoia.com>
2018-08-10 15:24:36 -0700

    Finish tabs placement but not functionality

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -5,13 +5,7 @@ import BuySell from './buy_sell';
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
-    // this.select = this.select.bind(this);
   }
-
-  // select(e) {
-  //   e.preventDefault();
-  //   document.getElementsByTagName('Dashboard')
-  // }
 
   render () {
     return (
@@ -19,13 +13,9 @@ class Navbar extends React.Component {
         <div className='empty-nav'></div>
         <div className='navbar-inner-container'>
           <div className='navbar-left'>
-            {/* <span className='dashboard-container' onClick={this.select}> */}
               <Dashboard />
-            {/* </span> */}
             <div className='inner-empty-nav'></div>
-            {/* <span className='buy-sell-container' onClick={this.select}> */}
               <BuySell/>
-            {/* </span> */}
           </div>
           <div className='empty-nav'></div>
         </div>
