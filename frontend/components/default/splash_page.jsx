d1226ce5ff4f42d0be0c3b2580a50db9f55e5aaa
Rex Bodoia <rex@bodoia.com>
2018-08-10 23:38:20 -0700

    Add text to splash page

--- frontend/components/default/splash_page.jsx
+++ frontend/components/default/splash_page.jsx
@@ -55,25 +55,29 @@ class SplashPage extends React.Component {
 
   render () {
     return (
-      <div className='splash-container'>
-        <SignupHeader history={this.props.history} location={this.props.location}/>
+      <div>
+        <div className='splash-container'>
+          <SignupHeader history={this.props.history} location={this.props.location}/>
 
-        <div className='splash-content'>
-          <h1 className='splash-heading'>Buy and sell digital currency</h1>
-          <p className='splash-text'>Coincenter is the easiest and most trusted place to buy, sell, and manage your digital currency</p>
-        </div>
-
-        <form onSubmit={this.openModal} className='splash-form'>
-          <div className='splash-email-container'>
-            <input type='email' placeholder='Email address' className='splash-email' onChange={this.update}></input>
+          <div className='splash-content'>
+            <h1 className='splash-heading'>Buy and sell digital currency</h1>
+            <p className='splash-text'>Coincenter is the easiest and most trusted place to buy, sell, and manage your digital currency</p>
           </div>
 
-          <input type='submit' className='splash-button' value='Get Started'></input>
-        </form>
+          <form onSubmit={this.openModal} className='splash-form'>
+            <div className='splash-email-container'>
+              <input type='email' placeholder='Email address' className='splash-email' onChange={this.update}></input>
+            </div>
 
-        <h1>Create your digital currency portfolio today</h1>
+            <input type='submit' className='splash-button' value='Get Started'></input>
+          </form>
 
-        {this.renderModal()}
+          {this.renderModal()}
+        </div>
+        <div className='lower-splash'>
+          <h1>Create your digital currency portfolio today</h1>
+          <p>Coincenter has a variety of features that make it the best place to start trading</p>
+        </div>
       </div>
     );
   }
