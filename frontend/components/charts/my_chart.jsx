62bcc8207ca0eebd3e99a8d09256356725d9840d
Rex Bodoia <rex@bodoia.com>
2018-08-12 16:49:16 -0700

    Render a working chart

--- frontend/components/charts/my_chart.jsx
+++ frontend/components/charts/my_chart.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Chart from 'chart.js';
 import { LineChart, Line } from 'recharts';
 
 class MyChart extends React.Component {
@@ -8,14 +7,26 @@ class MyChart extends React.Component {
   }
 
   componentDidMount() {
-    this.props.getPrices();
+    if (Object.values(getState().entities.prices).length === 0) {
+      this.props.getPrices();
+    }
+  }
+
+  renderChart(data) {
+    if (Object.values(data).length !== 0){
+      return (
+        <LineChart width={1000} height={200} data={data.bitcoin}>
+          <Line type="monotone" dataKey="price" stroke="#8884d8" dot={false} />
+        </LineChart>
+      );
+    }
   }
 
   render () {
-    console.log(getState().entities.prices);
+    let data = getState().entities.prices
     return (
       <div>
-
+        {this.renderChart(data)}
       </div>
     );
   }
--- frontend/reducers/prices_reducer.js
+++ frontend/reducers/prices_reducer.js
@@ -1,7 +1,7 @@
 import { RECEIVE_PRICES } from '../actions/prices_actions';
 // import { merge } from 'lodash';
 
-const pricesReducer = (state = [], action) => {
+const pricesReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_PRICES:
