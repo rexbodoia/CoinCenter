6ce8918548e2711d3f2ae6b1fca848c12ce1cff2
Rex Bodoia <rex@bodoia.com>
2018-08-12 18:07:08 -0700

    Add balances to seed file for guest users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do |num|
-  User.create(f_name: 'guest', l_name: 'user', email: "guest#{num}@user.com", password: '12345678')
-end
-
 Coin.create(name: 'Ethereum', ticker_symbol: :ETH, current_price: 322.09)
 Coin.create(name: 'Bitcoin Cash', ticker_symbol: :BCH, current_price: 572.47)
 Coin.create(name: 'Litecoin', ticker_symbol: :LTC, current_price: 59.94)
 Coin.create(name: 'Bitcoin', ticker_symbol: :BTC, current_price: 6266.01)
+
+rand_gen = Random.new;
+
+100.times do |user_num|
+  User.create(f_name: 'guest', l_name: 'user', email: "guest#{user_num}@user.com", password: '12345678')
+  4.times do |coin_num|
+    Balance.create!(user_id: user_num + 1, coin_id: coin_num + 1, amount: rand_gen.rand(2000.0).round(2))
+  end
+end
