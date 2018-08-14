3f44136b17caa6b4c6c204c2a6e26f4f35002355
Rex Bodoia <rex@bodoia.com>
2018-08-14 10:20:53 -0700

    Sort chart data to be monthly

--- frontend/components/signed_in/dashboard/portfolio_chart.jsx
+++ frontend/components/signed_in/dashboard/portfolio_chart.jsx
@@ -4,9 +4,24 @@ import { AreaChart, Area, Line, XAxis, YAxis, Tooltip } from 'recharts';
 class PortfolioChart extends React.Component {
   constructor(props) {
     super(props);
+
     this.calculateBalanceHistory = this.calculateBalanceHistory.bind(this);
     this.retrieveBalances = this.retrieveBalances.bind(this);
+    this.findSelectedTimeframe = this.findSelectedTimeframe.bind(this);
+    this.changeTimeframe = this.changeTimeframe.bind(this);
     this.totalData = [];
+    this.timeframes = ['day', 'week', 'month', 'year', 'all'];
+
+    this.state = {
+      balances: props.balances,
+      prices: props.prices,
+      id: props.id,
+      'day': false,
+      'week': false,
+      'month': true,
+      'year': false,
+      'all': false
+    }
   }
 
   componentDidMount() {
@@ -15,24 +30,57 @@ class PortfolioChart extends React.Component {
     this.retrieveBalances();
   }
 
-  retrieveBalances() {
-    this.props.getBalances(this.props.id).then(() => this.calculateBalanceHistory());
+  findSelectedTimeframe() {
+    return this.timeframes.filter(timeframe => this.state[timeframe] == true);
   }
 
-  calculateBalanceHistory() {
+  retrieveBalances() {
+    let [selectedTime] = this.findSelectedTimeframe();
+
+    this.props.getBalances(this.props.id).then(() => this.calculateBalanceHistory(selectedTime));
+  }
+
+  changeTimeframe(period) {
+    let others = this.timeframes.filter(timeframe => timeframe !== period);
+
+    this.setState({[period]: true, [others[0]]: false, [others[1]]: false, [others[2]]: false, [others[3]]: false});
+  }
+
+  longestBalance(balances) {
+    let balanceValues = Object.values(balances);
+    let lengths = balanceValues.map(balance => balance.length);
+
+    return Math.max(lengths);
+  }
+
+  calculateBalanceHistory(timeframe) {
     let balances = this.props.balances;
 
-    for (let i = 0; i < balances.bitcoin.length; i++){
+    const longestBalance = this.longestBalance(balances);
+
+    const timeframes = {
+      day: 1,
+      week: 7,
+      month: 30,
+      year: 365,
+      all: longestBalance
+    }
+
+    this.totalData = [];
+
+    for (let i = 0; i < timeframes[timeframe]; i++){
       let amount = balances.bitcoin[i].amount + balances.bitcoinCash[i].amount + balances.ethereum[i].amount + balances.litecoin[i].amount;
 
       let date = balances.bitcoin[i].date;
 
       this.totalData.push({date, amount});
     }
+
     this.setState({ balances })
   }
 
   renderChart(data) {
+    console.log(data);
     if (data.length !== 0){
       return (
         <AreaChart width={1188} height={160} data={data}>
@@ -57,11 +105,11 @@ class PortfolioChart extends React.Component {
         </div>
         <ul className='portfolio-chart-time-frames'>
           <li>1H</li>
-          <li>1D</li>
-          <li>1W</li>
-          <li>1M</li>
-          <li>1Y</li>
-          <li>ALL</li>
+          <li onClick={(e) => this.changeTimeframe('day')}>1D</li>
+          <li onClick={(e) => this.changeTimeframe('week')}>1W</li>
+          <li onClick={(e) => this.changeTimeframe('month')}>1M</li>
+          <li onClick={(e) => this.changeTimeframe('year')}>1Y</li>
+          <li onClick={(e) => this.changeTimeframe('all')}>ALL</li>
         </ul>
         {this.renderChart(this.totalData)}
         <div className='portfolio-chart-dates'>
