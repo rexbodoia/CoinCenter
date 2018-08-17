af4d9184a4f3699cb29557dc8194581dfa18d652
Rex Bodoia <rex@bodoia.com>
2018-08-17 11:03:58 -0700

    Finish styling portfolio list

--- frontend/components/signed_in/dashboard/portfolio/portfolio_list_item.jsx
+++ frontend/components/signed_in/dashboard/portfolio/portfolio_list_item.jsx
@@ -8,9 +8,10 @@ class PortfolioListItem extends React.Component {
   render() {
     return (
       <div className="portfolio-list-item">
+        <img src={window.images[this.props.symbol]}></img>
         <h1>{this.props.coin}</h1>
         <h2>{this.props.proportion.toFixed(2)}%</h2>
-        <span>{this.props.currentBalance.toFixed(2)} {this.props.symbol}</span>
+        <span>{this.props.currentBalance.toFixed(4)} {this.props.symbol}</span>
         <h3>${this.props.currentValue.toFixed(2)}</h3>
       </div>
     );
